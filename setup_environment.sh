199a01962f32488c7086bf30f46874c5102ede8b
U-BOC\leong006 <leong006@99HQPLSU4499082.boc.ad.census.gov>
2025-04-08 14:21:53 -0400

    Removed further DAS-specific spark default in setup env.

--- setup_environment.sh
+++ setup_environment.sh
@@ -5,10 +5,6 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 REQUIREMENTS_FILE="requirements.txt"
 VENV="gls-venv"
 
-# Remove DAS-Specific Spark-Defaults set by the CFT + subsequent bootstraps:
-echo "Removing DAS-Specific Spark-Defaults..."
-grep -v -E "^(spark.executorEnv.LD_LIBRARY_PATH|spark.pyspark.python|spark.pyspark.driver.python|spark.archives)" "$SPARKS_DEFAULTS_CONF_FILE" > temp.conf && mv -f temp.conf "$SPARKS_DEFAULTS_CONF_FILE"
-
 # Remove any existing venvs
 echo "Removing any existing venvs (if any)..."
 rm -rf $SCRIPT_DIR/$VENV
